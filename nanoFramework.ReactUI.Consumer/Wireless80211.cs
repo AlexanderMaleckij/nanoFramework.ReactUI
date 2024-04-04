9ad414a035f9360e762d7e8ea79f9cea4f135b30
Aliaksandr Maletski <sash.maleczckij201334@gmail.com>
2024-04-04 23:43:21 +0400

    Initial commit

---
+++ nanoFramework.ReactUI.Consumer/Wireless80211.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.NetworkInformation;
+
+namespace nanoFramework.ReactUI.Consumer
+{
+    internal class Wireless80211
+    {
+        /// <summary>
+        /// Disable the Wireless station interface.
+        /// </summary>
+        public static void Disable()
+        {
+            Wireless80211Configuration wconf = GetConfiguration();
+            wconf.Options = Wireless80211Configuration.ConfigurationOptions.None | Wireless80211Configuration.ConfigurationOptions.SmartConfig;
+            wconf.SaveConfiguration();
+        }
+
+        /// <summary>
+        /// Get the Wireless station configuration.
+        /// </summary>
+        /// <returns>Wireless80211Configuration object</returns>
+        public static Wireless80211Configuration GetConfiguration()
+        {
+            NetworkInterface ni = GetInterface();
+            return Wireless80211Configuration.GetAllWireless80211Configurations()[ni.SpecificConfigId];
+        }
+
+        public static NetworkInterface GetInterface()
+        {
+            NetworkInterface[] Interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            // Find WirelessAP interface
+            foreach (NetworkInterface ni in Interfaces)
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                {
+                    return ni;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ nanoFramework.ReactUI/ResourcesServer.cs
@@ -0,0 +1,110 @@
+﻿using nanoFramework.ResourcesMetadata;
+using nanoFramework.Runtime.Native;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Resources;
+using System.Threading;
+
+namespace nanoFramework.ReactUI
+{
+    public sealed class ResourcesServer
+    {
+        private const int ResourceChunkReadSize = 8192;
+        private readonly HttpListener _listener;
+        private readonly ResourceManager _resourceManager;
+        private CancellationTokenSource _tokenSource;
+        private Thread _serverThread;
+
+        public ResourcesServer()
+        {
+            _listener = new HttpListener("http", 80);
+            _resourceManager = new ResourceManager("nanoFramework.ReactUI.Resources", typeof(ResourcesServer).Assembly);
+        }
+
+        public void Start()
+        {
+            Stop();
+
+            _tokenSource = new CancellationTokenSource();
+
+            _listener.Start();
+
+            _serverThread = new Thread(() => Serve(_listener, _resourceManager, _tokenSource.Token));
+
+            _serverThread.Start();
+        }
+
+        public void Stop()
+        {
+            _tokenSource?.Cancel();
+            _serverThread?.Join();
+            _listener.Stop();
+        }
+
+        private static void Serve(HttpListener listener, ResourceManager resourceManager, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var context = listener.GetContext();
+
+                    try
+                    {
+                        var uriPath = GetUriPath(context.Request);
+                        var resourceMetadata = ResourcesMetadataProvider.FindByUriPath(uriPath);
+
+                        if (resourceMetadata is null)
+                        {
+                            context.Response.StatusCode = 404;
+                        }
+                        else
+                        {
+                            context.Response.ContentLength64 = resourceMetadata.Size;
+                            context.Response.ContentType = resourceMetadata.ContentType;
+
+                            if (resourceMetadata.ContentEncoding != null)
+                            {
+                                context.Response.Headers.Set("Content-Encoding", resourceMetadata.ContentEncoding);
+                            }
+
+                            for (var position = 0; position < resourceMetadata.Size; position += ResourceChunkReadSize)
+                            {
+                                var chunk = (byte[])ResourceUtility.GetObject(resourceManager, resourceMetadata.Id, position, ResourceChunkReadSize);
+                                context.Response.OutputStream.Write(chunk, 0, chunk.Length);
+                            }
+
+                            context.Response.OutputStream.Flush();
+                        }
+                    }
+                    finally
+                    {
+                        context.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Resources Server: {ex.Message}\r\nStack = {ex.StackTrace}");
+                }
+            }
+        }
+
+        private static string GetUriPath(HttpListenerRequest request)
+        {
+            var resourceName = request.RawUrl;
+            var hashIndex = resourceName.IndexOf('#');
+
+            if (hashIndex != -1)
+            {
+                resourceName = resourceName.Substring(0, hashIndex);
+            }
+            else if (resourceName.EndsWith("/"))
+            {
+                resourceName += "index.html";
+            }
+
+            return resourceName;
+        }
+    }
+}
